3414ea484cc18e668805670461de3b8aad087aae
Henrik Qvigstad <henrik.qvigstad@cern.ch>
2012-02-21 14:56:18 +0100

    Cleaned up find aliroot.

--- cmake/Modules/FindALIROOT.cmake
+++ cmake/Modules/FindALIROOT.cmake
@@ -38,18 +38,21 @@ execute_process(
     OUTPUT_VARIABLE ALICE_TARGET
     OUTPUT_STRIP_TRAILING_WHITESPACE)
 # libs should be found in
-set(ALICE_LIB_DIR $ENV{ALICE_ROOT}/lib/tgt_${ALICE_TARGET})
+message(STATUS "FindALIROOT.cmake: searching for AliROOT in $ENV{ALICE_ROOT}/lib/tgt_${ALICE_TARGET}")
+set(ALIROOT_LIB_DIR $ENV{ALICE_ROOT}/lib/tgt_${ALICE_TARGET})
 
 
 # check to see if libESD.so is built
-find_library(ALIROOT_FOUND ESD ${ALICE_LIB_DIR})
+find_library(ALIROOT_FOUND ESD ${ALIROOT_LIB_DIR})
+message(STATUS ${ALIROOT_FOUND})
 if(NOT ALIROOT_FOUND)
   message(STATUS "AliROOT not found")
   if(ALIROOT_FIND_REQUIRED)
     message(FATAL_ERROR "AliROOT Required!")
   endif(ALIROOT_FIND_REQUIRED)
-elseif(NOT ALIROOT_FOUND)
-  message(STATUS "libESD found, assuming that complete aliroot build is found in \$ALICE_ROOT")
+else(NOT ALIROOT_FOUND) # if found
+  message(STATUS "libESD found, assuming that complete and installed aliroot build is found in \$ALICE_ROOT")
+  set(ALIROOT_FOUND TRUE)
 endif(NOT ALIROOT_FOUND)
 
 
@@ -57,10 +60,10 @@ endif(NOT ALIROOT_FOUND)
 
 # Main AliROOT (should maybe be split into sub-components like the detector modules)
 set(ALIROOT_INCLUDE "$ENV{ALICE_ROOT}/include/")
-set(ALIROOT_LIB "-L${ALICE_LIB_DIR} -lmicrocern -llhapdf -lpythia6")
-set(ALIROOT_LIB "${ALIROOT_LIBS} -lSTEERBase -lESD -lCDB -lRAWDatabase -lRAWDatarec -lAOD -lANALYSIS")
-set(ALIROOT_LIB "${ALIROOT_LIBS} -lSTEER -lRAWDatasim -lFASTSIM -lEVGEN -lAliPythia6 -lSTAT")
-set(ALIROOT_LIB "${ALIROOT_LIBS} -lhijing -lTHijing -lSTRUCT")
+set(ALIROOT_LIB "-L${ALIROOT_LIB_DIR} -lmicrocern -llhapdf -lpythia6")
+set(ALIROOT_LIB "${ALIROOT_LIB} -lSTEERBase -lESD -lCDB -lRAWDatabase -lRAWDatarec -lAOD -lANALYSIS")
+set(ALIROOT_LIB "${ALIROOT_LIB} -lSTEER -lRAWDatasim -lFASTSIM -lEVGEN -lAliPythia6 -lSTAT")
+set(ALIROOT_LIB "${ALIROOT_LIB} -lhijing -lTHijing -lSTRUCT")
 
 
 # *** Components ***
@@ -376,24 +379,14 @@ endif(ALIROOT_USE_MFT)
 
 
 
+# Final setting up of variables
 set(ALIROOT_INCLUDES ${ALIROOT_INCLUDE})
 set(ALIROOT_INCLUDE_DIRS ${ALIROOT_INCLUDE})
 set(ALIROOT_LIBS ${ALIROOT_LIB})
 set(ALIROOT_LIBRARIES ${ALIROOT_LIB})
 
+#message(STATUS "FindALIROOT.cmake: ALIROOT_FOUND: ${ALIROOT_FOUND}")
+#message(STATUS "FindALIROOT.cmake: ALIROOT_INCLUDES: ${ALIROOT_INCLUDES}")
+#message(STATUS "FindALIROOT.cmake: ALIROOT_LIBS: ${ALIROOT_LIBS}")
 
 
-# macro(FINDLIBS RESULTBOOL PATHTOLOOK ) # ARGN (libnames)
-#   set(LIBNAMELIST ${ARGN})
-#   foreach(libname ${LIBNAMELIST})
-#     find_library(FOUND_${libname} ${libname} ${PATHTOLOOK} )
-# #      message("finding  ${libname} in ${PATHTOLOOK}")
-# #      message("result: " ${FOUND_${LIBPATH}})
-#     set(LIBPATH ${libname})
-#     if(NOT FOUND_${libname})
-#       message(STATUS "WARNING: lib ${libname} not found in ${PATHTOLOOK}")
-#       return()
-#     endif(NOT FOUND_${libname})
-#   endforeach(libname ${LIBNAMELIST})
-#   set(${RESULTBOOL} TRUE)
-# endmacro(FINDLIBS)
