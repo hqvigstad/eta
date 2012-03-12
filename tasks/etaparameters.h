efe6e0bcc853429dc145c556d568c7c239da6ea3
Henrik Qvigstad <henrik.qvigstad@cern.ch>
2012-03-12 15:04:47 +0100

    added etaqparameters.h

---
+++ tasks/etaparameters.h
@@ -0,0 +1,17 @@
+#ifndef ETAPARAMETERS_H
+#define ETAPARAMETERS_H
+
+namespace EtaParameters {
+
+  Int_t kEtap = 331;
+
+  enum Verbose { SILENT=0,
+		 FLOW=10, // flow of analysis
+		 EVENT=20, // event level
+		 CANDIDATE=30, // candidate level
+		 TRACK=40 // Track, cluster
+  };
+  
+}
+
+#endif
